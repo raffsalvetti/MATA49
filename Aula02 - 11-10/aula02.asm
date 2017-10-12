413bdc3a1520167efca50dc2841e4ad0b1f20e37
raffsalvetti <raffaello.salvetti@gmail.com>
2017-10-11 21:19:33 -0300

    aula02

--- Aula02 - 11-10/aula02.asm
+++ Aula02 - 11-10/aula02.asm
@@ -1,12 +1,24 @@
 %include "io.inc"
-
+section .data
+    string  db  "MAS QUE RAIVA RPZ!",0
+    endstring db "THE END!",0
 section .text
 global CMAIN
 CMAIN:
     mov ebp, esp; for correct debugging
-    mov al, 0
-    mov ah, 0
-    mov al, 1
-    mov ebx, 0FFFFFFFFh
-    movzx bx, al
+    mov ecx, 18
+    mov ebx, string
+    jmp print
+    ; 00000000 00000000
+print:
+    dec ecx
+    jz end
+    PRINT_CHAR [ebx + ecx]
+    mov ax, [ebx + ecx]
+    PRINT_DEC 2, ax
+    jmp print
+    
+end:
+    NEWLINE
+    PRINT_STRING endstring
     ret
