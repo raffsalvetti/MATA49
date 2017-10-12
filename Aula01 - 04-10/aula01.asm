22627247e6a85fec1cdbb98b39f1e4416512f0d6
raffsalvetti <raffaello.salvetti@gmail.com>
2017-10-11 21:55:32 -0300

    atualizacao da aula01 com comentarios

--- Aula01 - 04-10/aula01.asm
+++ Aula01 - 04-10/aula01.asm
@@ -4,22 +4,19 @@ section .text
 
 global CMAIN
 
-
-
 CMAIN:
-    mov ebp, esp; for correct debugging
-    call zero
-    mov eax, (10 << 2)
-    mov ebx, 100
-    add eax, ebx
-    PRINT_DEC 4, eax;
-    NEWLINE
-    PRINT_DEC 4, ebx;
+    mov             ebp,    esp     ; for correct debugging
+    xor             eax,    eax     ; fazendo xor de dois valores iguais resulta em zero. o objetivo é zerar os registradores
+    mov             ebx,      0     ; equivalente a "xor eax, eax", move o valor zero para ebx
+    mov              ch,      0     ; parte "alta" (h de high) do registrador c(de 8 bits)
+    mov              cl,      0     ; parte "baixa" (l de low) do registrador c(de 8 bits)
+    mov              cx,    136     ; movendo o valor 136 (1000 1000) para o registrador cx (composto por ch+cl)
+    mov             edx,    500     ; movendo o valor 500 para o registrador edx - extended dx ou 16bits(que é a parte extendida) + dh + dl = 32bits
+    add             ebx,    edx     ; somando o valor de edx a ebx, equiv a fazer ebx = ebx + edx em C
+    PRINT_DEC         2,    ebx     ; macro que imprime em stdout um valor decimal de 2bytes
+    NEWLINE                         ; imprime \n
+    add             ebx,    edx     ; executando a soma anterior
+    PRINT_DEC         2,    ebx
     ret
-    
-zero:
-    mov eax, 0
-    mov ebx, 0
-    mov ecx, 0
-    mov edx, 0
-    ret    
+
+delay:
