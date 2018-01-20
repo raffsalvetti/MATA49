5ce4954f2219a2eaa1f65ad054f0bd4e38159beb
Raffaello <raffaello.salvetti@gmail.com>
2018-01-20 19:18:30 -0300

    corrigido bug no ex2

--- Aula08 - Macros/exercicio2.asm
+++ Aula08 - Macros/exercicio2.asm
@@ -3,18 +3,22 @@
 ;;  Escreva uma macro que calcule a media de
 ;;  tres numeros e imprima o resultado no monitor
 
-section .text
-
-%macro soma3 3          ;   definindo nome da macro e numero de parametros
-    mov ax, %1          ;   movendo o valor do paramerto 1 para ax
-    add ax, %2          ;   ax += parametro 2
-    add ax, %3          ;   ax += parametro 3
-    PRINT_DEC 2, ax     ;   imprimindo o valor de ax
+%macro media3 3         ;   definindo nome da macro e numero de parametros
+    mov eax, %1          ;   movendo o valor do paramerto 1 para ax
+    add eax, %2          ;   ax += parametro 2
+    add eax, %3          ;   ax += parametro 3
+    mov edx, 0
+    mov ecx, 3
+    div ecx
+    PRINT_DEC 2, eax     ;   imprimindo o valor de ax
 %endmacro
+
+section .text
 
 global CMAIN
 CMAIN:
+    mov ebp, esp; for correct debugging
     ;write your code here
     xor eax, eax
-    soma3 1, 2, 3       ;   chamando o macro
-    ret                 ;   fim de programa
+    media3 1, 2, 3       ;   chamando o macro
+    ret                  ;   fim de programa
